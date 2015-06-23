c6981f0700aed79ea4b64844a512904c74c7e906
Quoc-Anh <ingenieur.de.logiciel@gmail.com>
2015-06-23 15:41:05 +0200

    user interface

---
+++ src/main/java/info/lequocanh/gembro/controller/ExceptionHandlerControllerAdvice.java
@@ -0,0 +1,28 @@
+package info.lequocanh.gembro.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Created by quocanh on 23/06/15.
+ * <p>
+ * Contact: ing.dev.java@gmail.com
+ */
+@ControllerAdvice
+public class ExceptionHandlerControllerAdvice {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlerControllerAdvice.class);
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNoSuchElementException(NoSuchElementException e) {
+        return e.getMessage();
+    }
+
+}
--- src/main/webapp/WEB-INF/ftl/users.ftl
+++ src/main/webapp/WEB-INF/ftl/users.ftl
@@ -1,4 +1,4 @@
-<#-- @ftlvariable name="users" type="java.util.List<eu.kielczewski.example.domain.User>" -->
+<#-- @ftlvariable name="users" type="java.util.List<info.lequocanh.gembro.domain.user.User>" -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
